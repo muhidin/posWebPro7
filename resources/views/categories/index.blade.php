49dc29f6e5ab30ed9924b672b6ee9afa00ced8aa
Muhidin <muhidins@gmail.com>
2025-04-18 22:49:50 +0700

    Category repair

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -21,92 +21,89 @@
                 </div>
             </div>
         </div>
-​
+        ​
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-4">
-                        @card
-                            @slot('title')
-                            Tambah
-                            @endslot
-                            
-                            @if (session('error'))
-                                @alert(['type' => 'danger'])
-                                    {!! session('error') !!}
-                                @endalert
-                            @endif
-​
+                        <div class="card">
+                            <div class="card-header with-border">
+                                <h3 class="card-title">Tambah Kategori</h3>
+                            </div>
+                            <div class="card-body">
+                                @if (session('error'))
+                                    <div class="alert alert-danger alert-dismissible">
+                                        {!! session('error') !!}
+                                    </div>
+                                @endif
+                            </div>
+                            ​
                             <form role="form" action="{{ route('categories.store') }}" method="POST">
                                 @csrf
                                 <div class="form-group">
-                                    <label for="name">Kategori</label>
-                                    <input type="text" 
-                                    name="name"
-                                    class="form-control {{ $errors->has('name') ? 'is-invalid':'' }}" id="name" required>
+                                    <label for="name">Nama Kategori</label>
+                                    <input type="text" name="name"
+                                        class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" id="name"
+                                        required>
                                 </div>
                                 <div class="form-group">
                                     <label for="description">Deskripsi</label>
-                                    <textarea name="description" id="description" cols="5" rows="5" class="form-control {{ $errors->has('description') ? 'is-invalid':'' }}"></textarea>
+                                    <textarea name="description" id="description" cols="5" rows="5"
+                                        class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}"></textarea>
                                 </div>
-                            @slot('footer')
                                 <div class="card-footer">
                                     <button class="btn btn-primary">Simpan</button>
+                                    <input type="reset" class="btn btn-md btn-warning pull-right">
                                 </div>
                             </form>
-                            @endslot
-                        @endcard
+                        </div>
                     </div>
                     <div class="col-md-8">
-                        @card
-                            @slot('title')
-                            List Kategori
-                            @endslot
-                            
-                            @if (session('success'))
-                                @alert(['type' => 'success'])
-                                    {!! session('success') !!}
-                                @endalert
-                            @endif
-                            
+                        <div class="card">
+                            <div class="card-header with-border">
+                                <h3 class="card-title">Daftar Kategori</h3>
+                            </div>
+                            <div class="card-body">
+                                @if (session('error'))
+                                    <div class="alert alert-success alert-dismissible">
+                                        {!! session('success') !!}
+                                    </div>
+                                @endif
+                            </div>
                             <div class="table-responsive">
                                 <table class="table table-hover">
                                     <thead>
                                         <tr>
-                                            <td>#</td>
+                                            <td>No</td>
                                             <td>Kategori</td>
                                             <td>Deskripsi</td>
                                             <td>Aksi</td>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @php $no = 1; @endphp
                                         @forelse ($categories as $row)
-                                        <tr>
-                                            <td>{{ $no++ }}</td>
-                                            <td>{{ $row->name }}</td>
-                                            <td>{{ $row->description }}</td>
-                                            <td>
-                                                <form action="{{ route('categories.destroy', $row->id) }}" method="POST">
-                                                    @csrf
-                                                    <input type="hidden" name="_method" value="DELETE">
-                                                    <a href="{{ route('categories.edit', $row->id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
-                                                    <button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
-                                                </form>
-                                            </td>
-                                        </tr>
+                                            <tr>
+                                                <td>{{ $loop->index + 1 }}</td>
+                                                <td>{{ $row->name }}</td>
+                                                <td>{{ $row->description }}</td>
+                                                <td>
+                                                    <form action="{{ route('categories.destroy', $row->id) }}" method="POST">
+                                                        @csrf
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <a href="{{ route('categories.edit', $row->id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
+                                                        <button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                                                    </form>
+                                                </td>
+                                            </tr>
                                         @empty
-                                        <tr>
-                                            <td colspan="4" class="text-center">Tidak ada data</td>
-                                        </tr>
+                                            <tr>
+                                                <td colspan="4" class="text-center">Tidak ada data</td>
+                                            </tr>
                                         @endforelse
                                     </tbody>
                                 </table>
                             </div>
-                            @slot('footer')
-​
-                            @endslot
-                        @endcard
+                        </div>
                     </div>
                 </div>
             </div>
